2c1480e00556483ad829a26ad10a39c1179ed0ac
curtisleefulton <curtisleefulton@gmail.com>
2013-01-29 20:44:41 -0800

    update docs

--- api/PgMdAtom.h
+++ api/PgMdAtom.h
@@ -15,13 +15,13 @@
  * SYNOPSIS
  *
  *     #import "PgMdAtom.h"
- *
+ *     
  *     id<PgMdAtomParser> atomParser = [PgMdAtom getParser];
- *
+ *     
  *     NSString *locale = @"en_US";
  *     NSData *xml = [@"<feed/>" dataUsingEncoding:NSUTF8StringEncoding];
  *     NSError *error;
- * 
+ *     
  *     PgMdAtomFeed *feed = [atomParser parse:locale:xml:&error];
  */
 @interface PgMdAtom : NSObject
