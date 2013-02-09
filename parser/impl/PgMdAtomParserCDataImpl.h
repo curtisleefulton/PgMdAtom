1a75c02d8c0aef868562646040db9662ec60e1cd
curtisleefulton <curtisleefulton@gmail.com>
2013-02-09 15:09:55 -0800

    add cdata support

---
+++ parser/impl/PgMdAtomParserCDataImpl.h
@@ -0,0 +1,12 @@
+//
+// Created by curtis.fulton on 2/9/13.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import <Foundation/Foundation.h>
+
+
+@interface PgMdAtomParserCDataImpl : NSObject
+@end
---
+++ parser/impl/PgMdAtomParserCDataImpl.m
@@ -0,0 +1,15 @@
+//
+// Created by curtis.fulton on 2/9/13.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import "PgMdAtomParserCDataImpl.h"
+
+
+@implementation PgMdAtomParserCDataImpl {
+
+}
+
+@end
